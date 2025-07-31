a2ccbaa9998adf942b28580a96f187eeab2f864e
shelovesherbody <shelovesherbody@outlook.com>
2025-07-31 06:20:45 +0300

    Update

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set user
+git config user.name "shelovesherbody"
+git config user.email "shelovesherbody@outlook.com"
+
+# Commit and push
+git checkout main
+git add .
+git commit -m "Update"
+git push
